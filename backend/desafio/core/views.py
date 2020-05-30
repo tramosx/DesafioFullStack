7ab48ecca92af52506721dc3cbcf9244c4c61c1d
thiagoneriramos <thiagoneriramos@yahoo.com.br>
2020-05-30 18:26:39 -0300

    Adiciona view lista_reunioes para exibir lista de reunioes por funcionario

--- backend/desafio/core/views.py
+++ backend/desafio/core/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Reuniao
+from django.http import JsonResponse
 
 # Create your views here.
+def lista_reunioes(request, id):
+    reuniaoArray = []
+    reunioes_funcionario = Reuniao.objects.filter(funcionarios=id)
+            
+    if reunioes_funcionario:
+        for reuniao in reunioes_funcionario:
+            reuniaoObj = {}
+            reuniaoObj['id_reuniao'] = reuniao.id,
+            reuniaoObj['nome_reuniao'] = reuniao.nome,
+            reuniaoObj['data_reuniao'] = reuniao.data.strftime('%d/%m/%Y às %H:%M')
+
+            reuniaoArray.append(reuniaoObj)
+
+    return JsonResponse(reuniaoArray, safe=False)
