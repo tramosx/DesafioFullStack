504185273a058d6490192b42f42b62f5fca9006a
thiagoneriramos <thiagoneriramos@yahoo.com.br>
2020-05-30 18:04:40 -0300

    Tabela reuniao criada e registrada

--- backend/desafio/core/models.py
+++ backend/desafio/core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import date
 
 # Create your models here.
+class Reuniao(models.Model):
+    nome = models.CharField(max_length=150)
+    data = models.DateTimeField()
+    foto = models.ImageField()
+    objetivo = models.TextField()
+    funcionarios = models.ManyToManyField(User)
+
+    def __str__(self):
+        return '%s %s' % (self.nome, self.data)
