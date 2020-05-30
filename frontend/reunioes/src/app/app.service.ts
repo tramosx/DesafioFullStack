4f29b60ed0da07e6d45b06f9a7ad8d329d5c2c76
thiagoneriramos <thiagoneriramos@yahoo.com.br>
2020-05-30 20:52:41 -0300

    configuracoes para API django

---
+++ frontend/reunioes/src/app/app.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
+import { NativeStorage } from '@ionic-native/native-storage/ngx';
+
+@Injectable()
+export class AppService {
+
+  private apiRoot = 'http://localhost:8000/';
+  isLoggedIn = false;
+  token:any;
+  variavel:any;
+
+  constructor(
+    private http: HttpClient,
+    private storage: NativeStorage,
+  ) { }
+
+
+  getReunioesById(id: String) {
+    return this.http.get(this.apiRoot.concat('reunioes/'+id+'/'));
+  }
+
+
+  login(username: String, password: String) {
+    return this.http.post(this.apiRoot.concat('rest-auth/login/'),
+      {username: username, password: password}
+    ).pipe(
+      tap(token => {
+        this.storage.setItem('token', token)
+        .then(
+          () => {
+            console.log('Token armazenado');
+          },
+          error => console.error('Erro ao salvar item', error)
+        );
+        this.token = token;
+        this.isLoggedIn = true;
+        return token;
+      }),
+    );
+  }
+}
