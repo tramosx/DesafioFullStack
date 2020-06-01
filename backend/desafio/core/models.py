0fdd421b5d20ef8abcf64ad96c2a7aa210bd90c4
thiagoneriramos <thiagoneriramos@yahoo.com.br>
2020-06-01 00:24:07 -0300

    salva imagens na pasta images/

--- backend/desafio/core/models.py
+++ backend/desafio/core/models.py
@@ -1,14 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 from datetime import date
+from django.core.files.storage import FileSystemStorage
+
+fs = FileSystemStorage(location='/home/thiagoneri/desafio_tecnico/DesafioFullStack/backend/desafio/core/static')
 
 # Create your models here.
 class Reuniao(models.Model):
     nome = models.CharField(max_length=150)
     data = models.DateTimeField()
-    foto = models.ImageField()
+    foto = models.ImageField(upload_to='images/')
     objetivo = models.TextField()
     funcionarios = models.ManyToManyField(User)
 
     def __str__(self):
-        return '%s %s' % (self.nome, self.data) 
+        return '%s %s %s' % (self.nome, self.data, self.foto) 
