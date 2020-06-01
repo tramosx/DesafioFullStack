ab9ae4e79f904880148bddd6acbfe386d82ab8bd
thiagoneriramos <thiagoneriramos@yahoo.com.br>
2020-06-01 00:24:50 -0300

    Adiciona url para campo foto

--- backend/desafio/core/views.py
+++ backend/desafio/core/views.py
@@ -1,20 +1,30 @@
 from django.shortcuts import render
 from .models import Reuniao
 from django.http import JsonResponse
+import json
 
 # Create your views here.
 def lista_reunioes(request, id):
     reuniaoArray = []
     reunioes_funcionario = Reuniao.objects.filter(funcionarios=id)
+
+    url = "http://127.0.0.1:8000/media/"
             
     if reunioes_funcionario:
         for reuniao in reunioes_funcionario:
             reuniaoObj = {}
             reuniaoObj['id_reuniao'] = reuniao.id,
+            reuniaoObj['foto'] = url + json.dumps(str(reuniao.foto)).strip('\"') 
             reuniaoObj['nome_reuniao'] = reuniao.nome,
             reuniaoObj['data'] = reuniao.data.strftime('%d/%m/%Y')
             reuniaoObj['hora'] = reuniao.data.strftime('%H:%M')
-
+            
             reuniaoArray.append(reuniaoObj)
 
+
+
+
+    print("reuniao")
+    print(reuniao)
+    print(request.build_absolute_uri)
     return JsonResponse(reuniaoArray, safe=False)
