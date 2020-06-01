5f744c1d1d95c3f7b0c9867b1b79f478f61aa11f
thiagoneriramos <thiagoneriramos@yahoo.com.br>
2020-06-01 17:51:01 -0300

    Adiciona postgresql

--- backend/desafio/desafio/settings.py
+++ backend/desafio/desafio/settings.py
@@ -86,10 +86,19 @@ WSGI_APPLICATION = 'desafio.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME', 'django'),
+        'USER': os.environ.get('DB_USER', 'admin'),
+        'PASSWORD': os.environ.get('DB_PASS', '123'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
+
 }
 
 
